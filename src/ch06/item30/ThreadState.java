050ce99ae912e1464d34e25f7a04bf5fb9353150
omottec <omottec@gmail.com>
2017-08-29 10:52:49 +0800

    enum is a special class

--- src/ch06/item30/ThreadState.java
+++ src/ch06/item30/ThreadState.java
@@ -3,5 +3,18 @@ package ch06.item30;
 /**
  * Created by qinbingbing on 29/08/2017.
  */
-public class ThreadState {
+public final class ThreadState {
+    private ThreadState() {}
+
+    public static final ThreadState NEW = new ThreadState();
+
+    public static final ThreadState RUNNABLE = new ThreadState();
+
+    public static final ThreadState BLOCKED = new ThreadState();
+
+    public static final ThreadState WAITING = new ThreadState();
+
+    public static final ThreadState TIMED_WAITING = new ThreadState();
+
+    public static final ThreadState TERMINATED = new ThreadState();
 }
